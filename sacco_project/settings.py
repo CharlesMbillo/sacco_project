075e509aa30130eecad9bb82c6544c0c0e173942
Charles Mbillo <mbillocharles@gmail.com>
2024-04-23 15:12:15 +0300

    CSRF commit

--- sacco_project/settings.py
+++ sacco_project/settings.py
@@ -40,12 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'transactions',
 ]
+CSRF_COOKIE_SECURE = False 
 
 MIDDLEWARE = [
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
